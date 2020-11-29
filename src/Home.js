6927c7712d19a47822fa3765a832d6a6f7b84ee8
bwoodie5150 <you@example.com>
2020-11-29 12:29:42 -0500

    home changes

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,10 @@ class Home extends Component
         return (
             <div>
                 <h2>Welcome to MGBD Parts & Service</h2> 
-                
+                <figure>
+                <img src="https://www.telegraph.co.uk/multimedia/archive/01808/Rover-P6_1808983c.jpg" alt="" style="width:100%">
+                <figcaption>Mark & Angie enjoying their Rover P6</figcaption></img>
+                </figure>
             </div>
         )
     }
